f9f3aac188a12a938b1b5e8895eb22c21af4c347
DarkEyeDragon <joshua.tack@student.vives.be>
2020-02-01 01:17:01 +0100

    Removed redundant if statement check

--- server/environment.js
+++ server/environment.js
@@ -4,7 +4,6 @@ const Collision = require('./collision');
 
 class Environment {
     static hitsBorder(player) {
-        if (player.snake[0].x > CONSTANTS.gridWidth - CONSTANTS.gridSize || player.snake[0].x < 0) return true;
         return player.snake[0].y > CONSTANTS.gridHeight - CONSTANTS.gridSize || player.snake[0].y < 0;
     }
 
@@ -21,40 +20,40 @@ class Environment {
         return collision;
     }
 
-//Check if you're hitting someones body, if so, you die.
-static hitsOther(player, socketList) {
-    for (let socket of socketList) {
-        let target = socket.player;
-        if(player !== target) {
-            const playerHead = player.snake[0];
-            const targetHead = target.snake[0];
-            if (playerHead.x === targetHead.x && playerHead.y === targetHead.y) {
-                let collision = new Collision(CONSTANTS.collision.HEAD_TO_HEAD);
-                collision.location = {x: playerHead.x, y: playerHead.y};
-                collision.player = player;
-                collision.target = target;
-                return collision;
-            }
-            for (let i = 0; i < target.snake.length; i++) {
-                if(playerHead.x === target.snake[i].x && playerHead.y === target.snake[i].y){
-                    let collision = new Collision(CONSTANTS.collision.HEAD_TO_BODY);
-                    collision.location = {x: target.snake[i].x, y: target.snake[i].y};
-                    collision.target = target;
+    //Check if you're hitting someones body, if so, you die.
+    static hitsOther(player, socketList) {
+        for (let socket of socketList) {
+            let target = socket.player;
+            if (player !== target) {
+                const playerHead = player.snake[0];
+                const targetHead = target.snake[0];
+                if (playerHead.x === targetHead.x && playerHead.y === targetHead.y) {
+                    let collision = new Collision(CONSTANTS.collision.HEAD_TO_HEAD);
+                    collision.location = {x: playerHead.x, y: playerHead.y};
                     collision.player = player;
+                    collision.target = target;
                     return collision;
+                }
+                for (let i = 0; i < target.snake.length; i++) {
+                    if (playerHead.x === target.snake[i].x && playerHead.y === target.snake[i].y) {
+                        let collision = new Collision(CONSTANTS.collision.HEAD_TO_BODY);
+                        collision.location = {x: target.snake[i].x, y: target.snake[i].y};
+                        collision.target = target;
+                        collision.player = player;
+                        return collision;
+                    }
                 }
             }
         }
+        let collision = new Collision(CONSTANTS.collision.HEAD_TO_BODY);
+        collision.isCollision = false;
+        return collision;
     }
-    let collision = new Collision(CONSTANTS.collision.HEAD_TO_BODY);
-    collision.isCollision = false;
-    return collision;
-}
 
     static getRanLocation() {
         return {
-            x: Math.floor((Math.random() * CONSTANTS.gridWidth/CONSTANTS.gridSize))*CONSTANTS.gridSize ,
-            y: Math.floor((Math.random() * CONSTANTS.gridHeight/CONSTANTS.gridSize))*CONSTANTS.gridSize
+            x: Math.floor((Math.random() * CONSTANTS.gridWidth / CONSTANTS.gridSize)) * CONSTANTS.gridSize,
+            y: Math.floor((Math.random() * CONSTANTS.gridHeight / CONSTANTS.gridSize)) * CONSTANTS.gridSize
         }
     };
 
